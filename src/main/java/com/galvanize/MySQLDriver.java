8136680226e743d800b45c22b11390e078312494
Marilene Soares da Costa <lene626@gmail.com>
2022-08-12 21:16:52 -0600

    passes tests

--- src/main/java/com/galvanize/MySQLDriver.java
+++ src/main/java/com/galvanize/MySQLDriver.java
@@ -2,23 +2,25 @@ package com.galvanize;
 
 import static java.lang.String.format;
 
-public class MySQLDriver {
-
-    public String quoteMySQLColumn(String column) {
+public class MySQLDriver implements Driver{
+    @Override
+    public String quoteColumn(String column) {
         if (column.contains(".")) {
             String tableName = column.substring(0, column.indexOf("."));
             String columnName = column.substring(column.indexOf(".") + 1);
-            return format("%s.`%s`", quoteMySQLTable(tableName), columnName);
+            return format("%s.`%s`", quoteTable(tableName), columnName);
         } else {
             return format("`%s`", column);
         }
     }
-
-    public String quoteMySQLTable(String table) {
+    @Override
+    public String quoteTable(String table) {
         return format("`%s`", table);
     }
 
-    public String quoteMySQLValue(String value) {
+
+    @Override
+    public String quoteValue(String value) {
         return format("\"%s\"", value);
     }
 
